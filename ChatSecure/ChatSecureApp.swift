5fd4d700a557b14d1dd625fd60895f43d900f7cb
arnavgupta1000 <arnavgupta1812@outlook.com>
2024-07-12 22:14:42 +0530

    Initial Commit

---
+++ ChatSecure/ChatSecureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSecureApp.swift
+//  ChatSecure
+//
+//  Created by Arnav Gupta on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatSecureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatSecure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatSecure
+//
+//  Created by Arnav Gupta on 12/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
